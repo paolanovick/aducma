b605b7f3f438b3b3336ceb8fb2554f3bef32def8
paolanovick <paola.novick@davinci.edu.ar>
2026-01-29 18:49:10 -0300

    imagenes 2

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -19,6 +19,24 @@ export default function Servicios() {
       img: "https://www.freepik.es/foto-gratis/mujer-tiro-medio-posando-hoja_13436448.htm#fromView=search&page=2&position=15&uuid=a748e400-463c-4d7e-b7a0-83908972afd6&query=ambiente+natural",
       url: "https://example.com/three",
       height: 600,
+      },
+     {
+      id: "4",
+      img: "https://www.freepik.es/foto-gratis/tomada-vertical-hermosa-toma-bosque-arboles-altos-sol-brillando-fondo_8280858.htm#fromView=search&page=1&position=12&uuid=a748e400-463c-4d7e-b7a0-83908972afd6&query=ambiente+natural",
+      url: "https://example.com/one",
+      height: 400,
+    },
+    {
+      id: "5",
+      img: "https://www.freepik.es/foto-gratis/viajero-asiatico-joven-feliz-mujer-mochila-que-camina-bosque_4014385.htm#fromView=search&page=1&position=27&uuid=a748e400-463c-4d7e-b7a0-83908972afd6&query=ambiente+natural",
+      url: "https://example.com/two",
+      height: 250,
+    },
+    {
+      id: "6",
+      img: "https://www.freepik.es/foto-gratis/mujer-tiro-medio-posando-hoja_13436448.htm#fromView=search&page=2&position=15&uuid=a748e400-463c-4d7e-b7a0-83908972afd6&query=ambiente+natural",
+      url: "https://example.com/three",
+      height: 600,
     },
   ];
 
